80f66b5c6f2a288d5018c9e5dba7caa349256d37
TsalkoIlja <iljatsalko9@gmail.com>
2025-10-30 10:14:20 +0200

    SwitchMultiple

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿namespace switchMultiple
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            //sisestad konsooli väärtuse
+            //loeb sisestatud vüürtuse stringina
+            //tuleb kasutada switchi
+
+            //esimeses cases on väärtused "Intro to C#", "Variables",
+            //"Date Types" ja vastuseks Basic
+
+            //teises cases on vääärtused "OOP", "Classes",
+            //"objects" ja vastuseks Intermediate
+
+            //kolmandas cases on väärtused "Ancynchrouns Programming", "LINQ",
+            //"Delegates" ja vastuseks Advanced
+            Console.WriteLine("Sisesta vüäärtus");
+            string input = Console.ReadLine();
+            switch (input)
+            {
+                case "Intro to C#":
+                case "variables":
+                case "Date types":
+
+                    Console.WriteLine("Basic");
+                    break;
+                case "OOP":
+                case "Classes":
+                case "Object":
+
+                    Console.WriteLine("intermediate");
+                    break;
+                case "Ancynchrouns Programming":
+                case "LINQ":
+                case "Delegates":
+
+                    Console.WriteLine("Advanced");
+                    break;
+
+
+
+
+            }
+        }
+    }
+}
