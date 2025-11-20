fdb8d0ba9a48eed0b9fde160be6377a5b3c1ce34
TsalkoIlja <iljatsalko9@gmail.com>
2025-11-20 10:44:05 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,10 +1,19 @@
-﻿namespace SwitchTryCatch
+﻿namespace WhileDo
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("While Do");
+            int i = 0;
+            do
+            {
+                Console.WriteLine();
+                // i++ tahendab et sama mis i=i+1
+                i++;
+
+            } while (i <= 10);
+            //kui tuli nr 11, siis tingimus et kehti enam ja tsükkel lõpeb
         }
     }
 }
