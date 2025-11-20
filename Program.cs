bf8a8de6eeab8e0bcdf3a8a659c3e46360aeab9f
TsalkoIlja <iljatsalko9@gmail.com>
2025-11-20 09:58:31 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,61 +1,10 @@
-﻿using System;
-
-namespace SwitchCalculator
+﻿namespace SwitchTryCatch
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("sisesta esimene number");
-            //loeme kasutaja sisendit ja see peab olema float andmetüüp
-
-            //konsool kirjutab, et sisesta teine number
-            //loeme kasutaja sisendit ja see peab olema float andmeetüüp
-            //mõlemal juhul tuleeb float ära parsida
-            float Firstnumber = float.Parse(Console.ReadLine());
-            Console.WriteLine(1);
-
-            float Secondnumber = float.Parse(Console.ReadLine());
-            Console.WriteLine(2);
-
-            Console.WriteLine("Esimene number" + Firstnumber);
-            Console.WriteLine("Teine number" + Secondnumber);
-
-            //konsool kirjutab, et vali tehe:  +,-,*,/
-            Console.WriteLine("Vali tehe: +,-,*,/");
-            string input = Console.ReadLine();
-
-            //kasutate switchi, et teha valik kasutraja sisendi põhjal
-
-            switch (input)
-            {
-                case "+":
-                    Console.WriteLine($"Tulemus:{Firstnumber + Secondnumber}");
-                    break;
-                case "-":
-                    Console.WriteLine($"Tulemus:{Firstnumber - Secondnumber}");
-                    break;
-                case "*":
-                    Console.WriteLine($"Tulemus:{Firstnumber * Secondnumber}");
-                    break;
-                case "/":
-                    if (Secondnumber == 0)
-                    {
-                        Console.WriteLine("Nulliga jagamine ei ole lubatud");
-                    }
-                    else
-                    {
-                        Console.WriteLine($"Tulemus:{Firstnumber / Secondnumber}");
-                    }  
-                        break;
-
-                default:
-                    Console.WriteLine("Teha pole lubatud");
-                    break;
-
-
-
-            }
+            Console.WriteLine("Hello, World!");
         }
     }
 }
