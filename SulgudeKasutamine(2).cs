85f70ca623a5a2ff4841e95ae323445cd4ca477b
TsalkoIlja <iljatsalko9@gmail.com>
2025-11-27 11:29:26 +0200

    Add files via upload

---
+++ SulgudeKasutamine(2).cs
@@ -0,0 +1,21 @@
+﻿namespace SulgudeKasutamine
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sulgude kasutamine");
+            Console.WriteLine("Kui palju on: 3 + 8 / (4 - 2) * 4 = ?");
+
+            double calculation1 = 3 + 8 / (4 - 2) * 4;
+            float calculation2 = 3 + 8 / 4 - 2 * 4;
+            float calculation3 = (3 + 8) / (4 - 2) * 4;
+            float calculation4 = 3 + 8 / ((4 - 2) * 4);
+
+            Console.WriteLine("3 + 8 / (4 - 2) * 4 = " + calculation1);
+            Console.WriteLine("3 + 8 / 4 - 2 * 4 = " + calculation2);
+            Console.WriteLine("(3 + 8) / (4 - 2) * 4 = " + calculation3);
+            Console.WriteLine("3 + 8 / ((4 - 2) * 4) = " + calculation4);
+        }
+    }
+}
