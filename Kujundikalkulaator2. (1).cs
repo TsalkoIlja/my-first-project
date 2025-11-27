9d7fdba4278695f78c3bf17b3defed3444b41591
TsalkoIlja <iljatsalko9@gmail.com>
2025-11-27 11:06:32 +0200

    Add files via upload

---
+++ Kujundikalkulaator2. (1).cs
@@ -0,0 +1,25 @@
+﻿using System.Net.NetworkInformation;
+
+namespace KujundiKalkulaator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Kujundi kalkulaator");
+
+            // Ruudu sees asub ring.Ringi raadius on 3 ühikut.
+            //Leia ja väljasta ekraanile ruudu pindala ja ümbermõõt.
+
+            int roundRadius = 3;
+            //int roundDiameter = 6;
+
+            Console.WriteLine("Ruudu pindala: " + (2 * roundRadius) * (2 * roundRadius));
+            Console.WriteLine("Ruudu ümbermõõt: " + (2 * roundRadius) * 4);
+
+            //Leia ja väljasta ekraanile ringi pindala ja ümbermõõt.
+            Console.WriteLine("Ringi pindala: " + Math.PI * (roundRadius * roundRadius));
+            Console.WriteLine("Ringi õmbermõõt: " + 2 * Math.PI * roundRadius);
+        }
+    }
+}
