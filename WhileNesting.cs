530a7d8399946b9cec1ee2dd58ff71fffb0545f7
TsalkoIlja <iljatsalko9@gmail.com>
2025-11-27 09:31:24 +0200

    Add files via upload

---
+++ WhileNesting.cs
@@ -0,0 +1,33 @@
+﻿namespace WhileNesting
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        { 
+            Console.WriteLine("Hello, World!");
+
+            //tuleb kasutada while ja omakorda while while sisse panna
+            //while nesting 
+            //while kordab ennast 5 korda 
+            int input = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("While nesting");
+
+
+            int a = 1;
+            while (a <= 5)
+            {
+                int b = 1;
+                while (b <= 5)
+                {
+                    Console.WriteLine(b + " ");
+                    b++;
+                }
+                Console.WriteLine();
+                a++;
+            } 
+
+
+        }
+    }
+}
