c853789c5dfbcabc7ba2354555512127bb490279
TsalkoIlja <iljatsalko9@gmail.com>
2025-12-04 11:39:48 +0200

    Add files via upload

---
+++ ForLoopAdding.cs
@@ -0,0 +1,15 @@
+﻿namespace ForLoopAdding
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            for (int i = 0; i < 10; i++)
+            {
+                sum += i;
+                Console.WriteLine($"Summa on:"+sum);
+            }
+        }
+    }
+}
