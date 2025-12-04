19a04b3a17cb806fdbc3363fb68c0dccbcdbeff4
TsalkoIlja <iljatsalko9@gmail.com>
2025-12-04 10:06:16 +0200

    Add files via upload

---
+++ FoorLoopHello.cs
@@ -0,0 +1,24 @@
+﻿namespace FoorLoopHello
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //teha konsoolirakendus, kus kasutad for loopi
+            //kolm korda näidatakse Tere tulemast
+            Console.WriteLine("hello");
+            ushort Tere = 0;
+
+            for (Tere = 0; Tere < 3; Tere++)
+            {
+                Console.WriteLine("Tere");
+                if(Tere == 3)
+                {
+                    break;
+                }
+
+
+            }
+        }
+    }
+}
