943d6ed7d8cd5a678c8f2c11b8b624ac7d7d7cfc
TsalkoIlja <iljatsalko9@gmail.com>
2025-12-04 09:40:28 +0200

    Add files via upload

---
+++ FoorLoopEndLess.cs
@@ -0,0 +1,29 @@
+﻿namespace FoorLoopEndLess
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("For loop endless");
+            //kasutame for loopiu
+            ushort i = 0;
+
+            //kui on 10 rida konsoolis, siis lõpetab ära
+            //VIHJE: kasutage break+i
+
+
+
+            for (i = 0;i < 10 ; i++ )
+            {
+                Console.WriteLine("Rida" + (i + 1));
+                if (i == 9)
+                {
+                    break;
+                    
+                }
+
+            }     
+
+        }
+    }
+}
