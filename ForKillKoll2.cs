76b60b682d0c50b50c40b1155a7228fb4b840dc2
TsalkoIlja <iljatsalko9@gmail.com>
2025-12-04 11:13:47 +0200

    Add files via upload

---
+++ ForKillKoll2.cs
@@ -0,0 +1,41 @@
+﻿namespace ForKillKoll
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            //Eelnevaid teadmisi kasutades kirjuta programm, mis väljastaks:
+            //kill - koll
+            //kill - koll
+            //killadi + kool
+            //kill - koll
+            //kill - koll
+            //killadi + kool
+            //kill - koll
+            //kill - koll
+            //killadi + kool
+            //kill - koll
+            //VIHJE: for loop for loopi sees
+
+            int arv = Convert.ToInt32(Console.ReadLine());
+
+        for (int i = 0; i < arv; i++)
+        {
+                Console.WriteLine("see on rida nt {0}",i);
+                
+            }
+            for (int j = 0; j < 2; j++)
+            {
+                Console.WriteLine("kill - koll");
+                
+            }
+            for (int k = 0; k < 1; k++)
+            {
+                Console.WriteLine("killandi - koll");
+               
+            }
+                 
+        }   
+    }
+}
