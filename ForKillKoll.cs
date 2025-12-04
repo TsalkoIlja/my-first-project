6efc23d6119391edc0826c3a6b4080308445f1a2
TsalkoIlja <iljatsalko9@gmail.com>
2025-12-04 10:56:52 +0200

    Add files via upload

---
+++ ForKillKoll.cs
@@ -0,0 +1,98 @@
+﻿namespace ForKillKoll
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            //Eelnevaid teadmisi kasutades kirjuta programm, mis väljastaks:
+            //kill - koll
+            //kill - koll
+            //killadi + kool
+            //kill - koll
+            //kill - koll
+            //killadi + kool
+            //kill - koll
+            //kill - koll
+            //killadi + kool
+            //kill - koll
+            //VIHJE: for loop for loopi sees
+            ushort i = 0;
+
+            for (i = 0; i < 1; i++)
+            {
+                Console.WriteLine("kill - koll");
+                if (i == 6)
+                {
+                    break;
+                }
+            }
+            for (i = 0; i < 1; i++)
+            {
+                Console.WriteLine("kill - koll");
+                if (i == 1)
+                {
+                    break;
+                }
+            }
+            for (i = 0; i < 1; i++)
+            {
+                Console.WriteLine("killandi - koll");
+                if (i == 1)
+                {
+                    break;
+                }
+            }
+            for (i = 0; i < 1; i++)
+            {
+                Console.WriteLine("kill - koll");
+                if (i == 6)
+                {
+                    break;
+                }
+            }
+            for (i = 0; i < 1; i++)
+            {
+                Console.WriteLine("kill - koll");
+                if (i == 1)
+                {
+                    break;
+                }
+            }
+            for (i = 0; i < 1; i++)
+            {
+                Console.WriteLine("killandi - koll");
+                if (i == 1)
+                {
+                    break;
+                }
+            }
+            for (i = 0; i < 1; i++)
+            {
+                Console.WriteLine("kill - koll");
+                if (i == 6)
+                {
+                    break;
+                }
+            }
+            for (i = 0; i < 1; i++)
+            {
+                Console.WriteLine("kill - koll");
+                if (i == 1)
+                {
+                    break;
+                }
+            }
+            for (i = 0; i < 1; i++)
+            {
+                Console.WriteLine("killandi - koll");
+                if (i == 1)
+                {
+                    break;
+
+               
+                }
+            }   
+        }
+    }
+}
