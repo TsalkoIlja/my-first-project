1cfc41969ef9697bcfe62a8ad2e78ebb6269879b
TsalkoIlja <iljatsalko9@gmail.com>
2025-12-04 09:45:15 +0200

    Add files via upload

---
+++ ForAsterisk.cs
@@ -0,0 +1,16 @@
+﻿namespace ForAsterisk
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("For Asterisk e *");
+            int nr = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < nr; i++)
+            {
+                Console.WriteLine("*");
+            }
+        }
+    }
+}
