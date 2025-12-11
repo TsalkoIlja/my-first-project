712ce52df30bc2447607180aff73a1e36015b02f
TsalkoIlja <iljatsalko9@gmail.com>
2025-12-11 10:49:28 +0200

    Add files via upload

---
+++ FotLoopMultiplicationTable.cs
@@ -0,0 +1,25 @@
+﻿namespace FotLoopMultiplicationTable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Korrutustabel");
+            int j, i, n;
+            Console.WriteLine("Sissesta tabelisse number, millest soovid alustada");
+            n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Korrutustabel ühest kuni {0}", n);
+            Console.WriteLine("\n");
+
+            for (i = 1; i < 10; i++)
+            {
+                for (j = 1; j <= n; j++)
+                {
+                    Console.Write("{0}x{1} = {2},", j, i, i * j);
+                }
+
+            }
+            Console.WriteLine("\n");
+        }
+    }
+}
