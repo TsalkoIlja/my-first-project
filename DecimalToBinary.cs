e1c05a2ce185e26672e4d263aa257cb5d16e404b
TsalkoIlja <iljatsalko9@gmail.com>
2025-12-17 13:17:04 +0200

    Add files via upload

---
+++ DecimalToBinary.cs
@@ -0,0 +1,31 @@
+﻿namespace DecimalToBinary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Decimal to binary");
+
+            Console.WriteLine("Siseta number");
+            int num = Convert.ToInt32(Console.ReadLine());
+            string binaryNumber = "";
+
+            //te peate kasutama while
+            //kümnendarvud tuleb binaararvudeks teisenda
+
+            while (num > 0)
+            {
+               
+                int remainder = num % 2;
+
+                
+                binaryNumber = remainder + binaryNumber;
+
+                
+                num /= 2;
+            }
+
+            Console.WriteLine(binaryNumber == "" ? "0" : binaryNumber);
+        }
+    }
+}
