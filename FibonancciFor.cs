d9d289129e78bb33d5f506e7517b12ec5cb41752
TsalkoIlja <iljatsalko9@gmail.com>
2025-12-17 14:21:24 +0200

    Add files via upload

---
+++ FibonancciFor.cs
@@ -0,0 +1,30 @@
+﻿namespace FibonacciFor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Fibanacci");
+            int prv = 0, pre = 1, trm, i, n;
+
+            Console.WriteLine("Sissesta number");
+
+            n = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"Siin on Fibonacci seeria kuni {0}: \n", n);
+            Console.Write($"{0}  {1}", prv, pre);
+
+            //kasutada for loopi, et teha algorütm
+
+            for (i = 2; i < n; i++)
+            {
+                trm = prv + pre;
+                Console.Write($"{trm} ");
+                prv = pre;
+                pre = trm;
+
+            }
+            Console.WriteLine();
+        }
+    }
+}
