76ee46dda23bb98e7822dc2ff8ae9d451713b399
TsalkoIlja <iljatsalko9@gmail.com>
2025-12-19 15:24:52 +0200

    Add files via upload

---
+++ ReatangleFor.cs
@@ -0,0 +1,29 @@
+﻿namespace RectangleFor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sisesta ristküliku külje mõõdud");
+
+            Console.WriteLine("Pikkus");
+            double leght = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Laius");
+            double witdh = double.Parse(Console.ReadLine());
+
+            //kasutada for loopi
+            //peab tuleb ristküliku kujund
+
+            for (int row = 0; row < leght; row++)
+            {
+                for (int col = 1; col < witdh; col++)
+                {
+                    Console.Write("* ");
+                }
+                Console.WriteLine();
+
+            }
+        }
+    }
+}
