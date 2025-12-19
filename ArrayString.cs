fb563c78543244f61c0c1d8f13b0599279be1f58
TsalkoIlja <iljatsalko9@gmail.com>
2025-12-19 15:38:42 +0200

    Add files via upload

---
+++ ArrayString.cs
@@ -0,0 +1,34 @@
+﻿namespace ArrayString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Massiiv");
+            string sentence = "Euroopa üks pealinnadest on: ";
+
+            //näide 1 Tuple
+            Console.WriteLine("Tuple");
+            var cities = Tuple.Create("Tallinn", "Pariis", "Rooma", "London", "Helsingi");
+            Console.WriteLine(sentence + cities.Item4);
+            Console.WriteLine("-----------------------------");
+
+            //näide 2 massiiv
+            string[] city = { "Tallinn", "Pariis", "Rooma", "London", "Helsingi" };
+            for (int i = 0; i < city.Length; i++)
+            {
+                Console.WriteLine(sentence + city[i]);
+            }
+            Console.WriteLine("-----------------------------");
+
+            //näide 3
+            Console.WriteLine(sentence + city[2]);
+            Console.WriteLine("-----------------------------");
+
+            //näide 4 Random
+            Random number = new Random();
+            int randomNumber = number.Next(0, 4);
+            Console.WriteLine(sentence + city[randomNumber]);
+        }
+    }
+}
