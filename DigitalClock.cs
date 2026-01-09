6b5526972e93f31fb3bd521f2a876ea0073b21e5
TsalkoIlja <iljatsalko9@gmail.com>
2026-01-09 13:48:35 +0200

    Add files via upload

---
+++ DigitalClock.cs
@@ -0,0 +1,51 @@
+﻿using System.Diagnostics;
+
+namespace DigitalClock
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Digital Clock";
+            Console.CursorVisible = false;
+
+            //while (true)
+            //{
+            //    Console.Clear();
+            //    Console.WriteLine("Digital Clock\n");
+            //    Console.WriteLine(DateTime.Now.ToString("HH:mm:ss"));
+            //    Thread.Sleep(1000);
+            //}
+
+            //ts[kkel
+            //while (true)
+            //{
+            //    //puhastab konsooli
+            //    Console.Clear();
+            //    //muutuja time, millel on arvutiaeg
+            //    string time = DateTime.Now.ToString("HH:mm:ss");
+            //    //kella horisontaalne asetus
+            //    int x = (Console.WindowWidth - time.Length) / 2;
+            //    //kella vertikaalne asetus
+            //    int y = Console.WindowHeight / 2;
+            //    //positsioon
+            //    Console.SetCursorPosition(x, y);
+            //    //näitab aega
+            //    Console.Write(time);
+            //    //peale 1 sek laseb tsükklil edasi minna
+            //    Thread.Sleep(1000);
+            //}
+            //ctrl + k + c
+
+            //for loopiga teha digitaalne kell
+
+            for ( ; ; )
+            {
+                Console.Clear();
+                Console.WriteLine("Digital Clock\n");
+                Console.WriteLine(DateTime.Now.ToString("HH:mm:ss"));
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
