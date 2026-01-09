3394e1d30eccc01305eae8ca8c6143adaea469a9
TsalkoIlja <iljatsalko9@gmail.com>
2026-01-09 15:13:33 +0200

    Add files via upload

---
+++ Square.cs
@@ -0,0 +1,29 @@
+﻿namespace Square
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sisesta ruudu suurus");
+            //tärnidega (*) teha for loopiga ruut
+            int size = Convert.ToInt32(Console.ReadLine());
+
+            
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    Console.Write("* ");
+                }
+                Console.WriteLine();
+            }
+            //kui suurus on sisestatud, siis kuvab meile ümbermõõdu ja pindala
+            Console.WriteLine("\n");
+            int area = size * size;
+            int circumreference = 4 * size;
+            Console.WriteLine("Pindala on " + area);
+            Console.WriteLine("Ümbermõõt on " + circumreference);
+
+        }
+    }
+}
