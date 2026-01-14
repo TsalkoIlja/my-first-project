f4a4f82c109c7323f0ea5ab0a00a112af4fbca1c
TsalkoIlja <iljatsalko9@gmail.com>
2026-01-14 14:14:58 +0200

    Add files via upload

--- MethodCall.cs
+++ MethodCall.cs
@@ -4,18 +4,69 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-            SayHello();
-            SayGoodbye();
+            Console.WriteLine("1. For loop");
+            Console.WriteLine("2. While loop");
+            Console.WriteLine("3. Liitmine");
+            Console.WriteLine("4. Vanus");
+            string choice = Console.ReadLine();
+
+            if (choice == "1")
+            {
+                ForLoop();
+            }
+            else if (choice == "2")
+            {
+                WhileLoop();
+            }
+            else if (choice == "3")
+            {
+                AddingNumbers();
+            }
+            else if (choice == "4")
+            {
+                AskAge();
+            }
+            else
+            {
+                Console.WriteLine("ERROR");
+            }
         }
 
-        static void SayHello()
+        static void ForLoop()
         {
-            Console.WriteLine("Hello");
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine("For Hello");
+            }
         }
-        static void SayGoodbye()
+
+        static void WhileLoop()
         {
-            Console.WriteLine("Goodbye");
+            int number = 1;
+
+            do
+            {
+                Console.WriteLine("While Hello");
+                number++;
+            }
+            while (number == 2);
+        }
+
+        static void AddingNumbers()
+        {
+            Console.WriteLine("Sisesta esimene number");
+            int first = int.Parse(Console.ReadLine());
+            Console.WriteLine("Sisesta teine number");
+            int second = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Vastus: " + (first + second));
+        }
+
+        static void AskAge()
+        {
+            Console.WriteLine("Sisesta enda vanus");
+            string age = Console.ReadLine();
+            Console.WriteLine("Sinu vanus on " + age);
         }
     }
 }
