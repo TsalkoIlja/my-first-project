c0ce9b3a75939acffb57ea9c746e06ed6fc8a8ae
TsalkoIlja <iljatsalko9@gmail.com>
2026-01-14 13:49:11 +0200

    Add files via upload

---
+++ AnimalLength.cs
@@ -0,0 +1,44 @@
+﻿
+namespace AnimalLength
+{
+    internal class Program
+    {
+        public static Func<string, object> GetNameLength { get; private set; }
+
+        static void Main(string[] args)
+        {
+            //Koer, kass, jänes, sipelgasiil ja lendorav hakkasid vaidlema,
+            //kellel on kõige pikem nimetus. Koosta programm, kus on antud loomanimede järjend.
+            //Loo funktsioon, mis saab argumendina ette looma nime ning tagastab (mitte ei trüki!)
+            //selle pikkuse. Lase programmil kutsuda funktsiooni välja
+            //iga loomanimede järjendi elemendiga ning trükib ekraanile nime pikkuse.
+            //Pärast pikkuste kontrollimist peab programm väljastama kõige pikema loomanime
+            //(võrdsete pikkuste puhul võib esitada vaid ühe pikimatest).'
+            //teha see töö väiksemateks ülesanneteks
+            string[] animalName = { "koer", "kass", "jänes", "sipelgasiil", "lendorav" };
+            //2. programm peab aru saama sõna pikkusest
+            int longerName = animalName[0].Length;
+            string longerNameString = animalName[0];
+            //3. for loop tuleb teha, et saaks nimeda pikkust omavahel võrrelda
+            //for loopi sees tuleks kasutada if-i
+            for (int i = 0; i < animalName.Length; i++)
+            {
+               if (longerName < animalName[i].Length)
+               {
+                    longerName = animalName[i].Length;
+                    longerNameString = animalName[i];
+               }
+                int animalLength = AnimalLength(animalName[i]);               
+
+                Console.WriteLine(animalName[i] + " = ");                
+                Console.WriteLine(animalLength);
+                Console.WriteLine();
+            }
+            Console.WriteLine("Pikim nimi oli {0} ja mille nime pikkus oli: {1}", longerNameString, longerName);
+        }
+        static int AnimalLength(string animalName)
+        {
+            return animalName.Length;
+        }
+    }   
+}
