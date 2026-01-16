ed480c7f6e69c592c15b63782aeca194c5875c42
TsalkoIlja <iljatsalko9@gmail.com>
2026-01-16 15:07:36 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿namespace FailWriter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Kirjuta faili läbi konsooli");
+
+            string filePath = @"C:/Users/ingva/Desktop/WriteToFile.txt";
+            string inputText = Console.ReadLine();
+
+            File.WriteAllText(filePath, inputText);
+        }
+    }
+}
